ebdb08c32829f97cc5f0f8687e20c12041aca81a
Mahdi Kalan <kalan.mahdi@yahoo.com>
2020-04-06 05:21:35 -0700

    securing routes

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 export default {
+    STRIPE_KEY: "pk_test_FMmmQztvXyn64HykozividSF00NlU6hGMF",
+    MAX_ATTACHMENT_SIZE: 5000000,
     s3: {
       REGION: "us-east-1",
       BUCKET: "notes-app-uploads-kalan"
